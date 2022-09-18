c243a252d58f03b6462277e40af8c987d03972a4
kevindiazp97 <kevindiazp97@gmail.com>
2022-09-18 22:31:32 +0700

    Add output count

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async() =>{
+    const IIX = await promiseTheaterIXX()
+
+    const VGC = await promiseTheaterVGC()
+    
+    const marahIIX = IIX.filter(function(item){
+      return item.hasil == 'marah'
+    })
+
+    const marahVGC = VGC.filter(function(item){
+      return item.hasil == 'marah'
+    })
+
+    const tmarahIIX = IIX.filter(function(item){
+      return item.hasil == 'tidak marah'
+    })
+
+    const tmarahVGC = VGC.filter(function(item){
+      return item.hasil == 'tidak marah'
+    })
+
+    console.log(marahIIX.length + marahVGC.length)
+
+    console.log(tmarahVGC.length + tmarahIIX.length)
+}
 
 module.exports = {
   promiseOutput,
